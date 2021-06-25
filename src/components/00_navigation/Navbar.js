d610b4bdda59e9b81e62dea7123542b9d6bee64f
src-dbgr <src.dbgr@gmail.com>
2021-06-25 09:17:07 +0200

    navbar start

---
+++ src/components/00_navigation/Navbar.js
@@ -0,0 +1,10 @@
+import React from "react";
+import triangle from "../../assets/images/navigation/triangle.svg";
+import circle from "../../assets/images/navigation/circle.svg";
+import PageLinks from "../../data/constants/links";
+
+const Navbar = () => {
+  return <PageLinks />;
+};
+
+export default Navbar;
