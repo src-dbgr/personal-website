022c5af3039634658908faae29b909cb8527eb81
src-dbgr <src.dbgr@gmail.com>
2021-06-28 03:34:05 +0200

    disabled animation for internet explorer

--- src/assets/animations/launch_anim.js
+++ src/assets/animations/launch_anim.js
@@ -1,198 +1,224 @@
 "use strict";
 
-let launchAnimation = function () {
-  let logoEl = document.querySelector("#logo");
-  let trianglePathEls = logoEl.querySelectorAll(
-    "#triangle polygon:not(#_12triangleback)"
-  );
-  let pathLength = trianglePathEls.length;
-  let hasStarted = false;
-  let aimations = [];
+function isIE() {
+  const ua = window.navigator.userAgent;
+  const msie = ua.indexOf("MSIE "); // IE 10 or older
+  const trident = ua.indexOf("Trident/"); //IE 11
+  return msie > 0 || trident > 0;
+}
 
-  let breathAnimation = anime({
-    begin: function () {
-      for (let i = 0; i < pathLength; i++) {
-        if (window.CP.shouldStopExecution(0)) break;
-        aimations.push(
-          anime({
-            targets: trianglePathEls[i],
-            stroke: {
-              value: ["rgba(150, 149, 141, 0.8)"],
-              duration: 1000,
-            },
-            strokeWidth: [0, 1.5],
-            translateX: [2, -4],
-            translateY: [2, -4],
-            opacity: [0.3, 1],
-            easing: "easeOutQuad",
-            autoplay: false,
-          })
-        );
-      }
-      window.CP.exitedLoop(0);
-    },
-    update: function (ins) {
-      aimations.forEach(function (animation, i) {
-        let percent = (1 - Math.sin(i * 0.35 + 0.0022 * ins.currentTime)) / 2;
-        animation.seek(animation.duration * percent);
-      });
-    },
-    duration: Infinity,
-    autoplay: false,
-  });
+function clickIE(e) {
+  document.querySelector("#triangle").removeEventListener("click", clickIE);
+  document.body.removeChild(document.getElementById("imagewrapper"));
+}
 
-  let introAnimation = anime
-    .timeline({
+if (isIE()) {
+  document.querySelector("#logo").style.opacity = 1;
+  document.getElementById("triangle").onclick = clickIE; //onclick event
+} else {
+  let launchAnimation = function () {
+    let logoEl = document.querySelector("#logo");
+    let trianglePathEls = logoEl.querySelectorAll(
+      "#triangle polygon:not(#_12triangleback)"
+    );
+    let pathLength = trianglePathEls.length;
+    let hasStarted = false;
+    let aimations = [];
+
+    let breathAnimation = anime({
+      begin: function () {
+        for (let i = 0; i < pathLength; i++) {
+          if (window.CP.shouldStopExecution(0)) break;
+          aimations.push(
+            anime({
+              targets: trianglePathEls[i],
+              stroke: {
+                value: ["rgba(150, 149, 141, 0.8)"],
+                duration: 1000,
+              },
+              strokeWidth: [0, 1.5],
+              translateX: [2, -4],
+              translateY: [2, -4],
+              opacity: [0.3, 1],
+              easing: "easeOutQuad",
+              autoplay: false,
+            })
+          );
+        }
+        window.CP.exitedLoop(0);
+      },
+      update: function (ins) {
+        aimations.forEach(function (animation, i) {
+          let percent = (1 - Math.sin(i * 0.35 + 0.0022 * ins.currentTime)) / 2;
+          animation.seek(animation.duration * percent);
+        });
+      },
+      duration: Infinity,
       autoplay: false,
-    })
-    .add({
-      targets: trianglePathEls,
-      strokeDashoffset: {
-        value: [anime.setDashoffset, 0],
-        duration: 3900,
-        easing: "easeInOutCirc",
-        delay: anime.stagger(190, { direction: "reverse" }),
-      },
-
-      duration: 2000,
-      delay: anime.stagger(60, { direction: "reverse" }),
-      easing: "linear",
-      complete: function (anim) {
-        introAnimation.remove();
-      },
     });
 
-  function startElementMotion() {
-    introAnimation.play();
-    breathAnimation.play();
-  }
-
-  function pauseElementMotion() {
-    introAnimation.pause();
-    breathAnimation.pause();
-  }
-
-  let tl_stop = anime.timeline({
-    easing: "easeOutExpo",
-    duration: 500,
-    autoplay: false,
-  });
-
-  tl_stop
-    .add({
-      targets: "#description, #outercircle",
-      rotate: [0, 360],
-      transformOrigin: ["50% 50% 0", "50% 60% 0"],
-      scale: 0.5,
-      opacity: 0,
-    })
-    .add({
-      targets: "#innercircle",
-      opacity: 0,
-      scale: 1.4,
-      transformOrigin: ["50% 50% 0", "50% 50% 0"],
-    })
-    .add({
-      targets: "#triangle polygon",
-      translateX: anime.stagger(10, {
-        grid: [1, -150],
-        from: "center",
-        axis: "x",
-      }),
-      translateY: anime.stagger(10, {
-        grid: [1, -150],
-        from: "center",
-        axis: "y",
-      }),
-      rotateZ: anime.stagger([0, 90], {
-        grid: [14, 5],
-        from: "center",
-        axis: "x",
-      }),
-      delay: function (el, i) {
-        return i * 100;
-      },
-      easing: "easeInOutSine",
-      complete: function (anim) {
-        tl_stop.remove();
-      },
-    });
-
-  let animTimeout = 1000;
-  let killAnimationTriggered = false;
-
-  function killAnimation() {
-    if (!killAnimationTriggered) {
-      document.querySelector("#triangle #_12triangleback").style.opacity = 0;
-      tl_stop.play();
-      pauseElementMotion();
-      document
-        .querySelector("#triangle")
-        .removeEventListener("click", killAnimation);
-      setTimeout(() => {
-        document.getElementById("imagewrapper").remove();
-      }, animTimeout * 3);
-      killAnimationTriggered = !killAnimationTriggered;
-    }
-  }
-
-  function startAnimation() {
-    let tl_start = anime.timeline({
-      easing: "easeOutExpo",
-      duration: animTimeout,
-    });
-
-    tl_start
-      .add({
-        targets: "#outercircle",
-        transformOrigin: ["50% 50% 0", "50% 50% 0"],
-        scale: [0, 0.5, 1],
-        opacity: 1,
+    let introAnimation = anime
+      .timeline({
+        autoplay: false,
       })
       .add({
-        targets: "#triangle,#innercircle",
-        transformOrigin: ["50% 50% 0", "50% 50% 0"],
-        opacity: [0, 0.2, 0.5, 0.95],
-        rotate: [0, 1080],
-        scale: [0, 0.2, 1.1, 1],
-      })
-      .add({
-        targets: "#description",
-        opacity: [0, 1],
-      })
-      .add({
-        targets: "#triangle",
-        transformOrigin: ["50% 55% 0", "50% 55% 0"],
-        rotate: [0, 720],
+        targets: trianglePathEls,
+        strokeDashoffset: {
+          value: [anime.setDashoffset, 0],
+          duration: 3900,
+          easing: "easeInOutCirc",
+          delay: anime.stagger(190, { direction: "reverse" }),
+        },
+
+        duration: 2000,
+        delay: anime.stagger(60, { direction: "reverse" }),
+        easing: "linear",
         complete: function (anim) {
-          tl_start.remove();
+          introAnimation.remove();
         },
       });
 
-    return tl_start;
-  }
+    function startElementMotion() {
+      introAnimation.play();
+      breathAnimation.play();
+    }
 
-  // kick off animation
-  function initialAnimation() {
-    // make background layer invisible
-    [
-      ...document.querySelectorAll(
-        "#description,#outercircle,#innercircle,#triangle,#_12triangleback"
-      ),
-    ].map((item) => {
-      item.style.opacity = 0;
+    function pauseElementMotion() {
+      introAnimation.pause();
+      breathAnimation.pause();
+    }
+
+    let tl_stop = anime.timeline({
+      easing: "easeOutExpo",
+      duration: 500,
+      autoplay: false,
     });
-    // in order to not render on load up make logo visible later
-    document.querySelector("#logo").style.opacity = 1;
-    let startAnim = startAnimation();
-    startAnim.play();
-    setTimeout(startElementMotion, animTimeout * 3);
-    setTimeout(killAnimation, animTimeout * 60000); // launch after timeout if user does not click to launch
-  }
 
-  document.querySelector("#triangle").onclick = killAnimation;
+    tl_stop
+      .add({
+        targets: "#description, #outercircle",
+        rotate: [0, 360],
+        transformOrigin: ["50% 50% 0", "50% 60% 0"],
+        scale: 0.5,
+        opacity: 0,
+      })
+      .add({
+        targets: "#innercircle",
+        opacity: 0,
+        scale: 1.4,
+        transformOrigin: ["50% 50% 0", "50% 50% 0"],
+      })
+      .add({
+        targets: "#triangle polygon",
+        translateX: anime.stagger(10, {
+          grid: [1, -150],
+          from: "center",
+          axis: "x",
+        }),
+        translateY: anime.stagger(10, {
+          grid: [1, -150],
+          from: "center",
+          axis: "y",
+        }),
+        rotateZ: anime.stagger([0, 90], {
+          grid: [14, 5],
+          from: "center",
+          axis: "x",
+        }),
+        delay: function (el, i) {
+          return i * 100;
+        },
+        easing: "easeInOutSine",
+        complete: function (anim) {
+          tl_stop.remove();
+        },
+      });
 
-  return initialAnimation;
-};
+    let animTimeout = 1000;
+    let killAnimationTriggered = false;
 
-launchAnimation()();
+    function killAnimation() {
+      if (!killAnimationTriggered) {
+        document.querySelector("#triangle #_12triangleback").style.opacity = 0;
+        tl_stop.play();
+        pauseElementMotion();
+        document
+          .querySelector("#triangle")
+          .removeEventListener("click", killAnimation);
+        setTimeout(function () {
+          document.getElementById("imagewrapper").remove();
+        }, animTimeout * 3);
+        killAnimationTriggered = !killAnimationTriggered;
+      }
+    }
+
+    function startAnimation() {
+      let tl_start = anime.timeline({
+        easing: "easeOutExpo",
+        duration: animTimeout,
+      });
+
+      tl_start
+        .add({
+          targets: "#outercircle",
+          transformOrigin: ["50% 50% 0", "50% 50% 0"],
+          scale: [0, 0.5, 1],
+          opacity: 1,
+        })
+        .add({
+          targets: "#triangle,#innercircle",
+          transformOrigin: ["50% 50% 0", "50% 50% 0"],
+          opacity: [0, 0.2, 0.5, 0.95],
+          rotate: [0, 1080],
+          scale: [0, 0.2, 1.1, 1],
+        })
+        .add({
+          targets: "#description",
+          opacity: [0, 1],
+        })
+        .add({
+          targets: "#triangle",
+          transformOrigin: ["50% 55% 0", "50% 55% 0"],
+          rotate: [0, 720],
+          complete: function (anim) {
+            tl_start.remove();
+          },
+        });
+
+      return tl_start;
+    }
+
+    // kick off animation
+    function initialAnimation() {
+      // make background layer invisible
+
+      let nodes = document.querySelectorAll(
+        "#description,#outercircle,#innercircle,#triangle,#_12triangleback"
+      );
+
+      Array.from(nodes).forEach(function (item) {
+        item.style.opacity = 0;
+      });
+
+      // remove in order to make it work in IE ...
+      // [
+      //   ...document.querySelectorAll(
+      //     "#description,#outercircle,#innercircle,#triangle,#_12triangleback"
+      //   ),
+      // ].map((item) => {
+      //   item.style.opacity = 0;
+      // });
+      // in order to not render on load up make logo visible later
+      document.querySelector("#logo").style.opacity = 1;
+      let startAnim = startAnimation();
+      startAnim.play();
+      setTimeout(startElementMotion, animTimeout * 3);
+      setTimeout(killAnimation, animTimeout * 60000); // launch after timeout if user does not click to launch
+    }
+
+    document.querySelector("#triangle").onclick = killAnimation;
+
+    return initialAnimation;
+  };
+  launchAnimation()();
+}
