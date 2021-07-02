037b8b7344696c994b4a0e4178106620163d6925
src-dbgr <src.dbgr@gmail.com>
2021-07-02 23:43:55 +0200

    fixed animation render condition in layout, removed sidebar button since it is not required

--- src/components/01_navigation/Sidebar.js
+++ src/components/01_navigation/Sidebar.js
@@ -5,9 +5,6 @@ import Links from "../../data/constants/links";
 const Sidebar = (props) => {
   return (
     <aside className={`sidebar ${props.sideBarIsOpen ? "show-sidebar" : ""}`}>
-      <button className="close-btn" onClick={props.toggleSideBar}>
-        <FaTimes />
-      </button>
       <div className="side-container">
         <Links styleClass={`${props.sideBarIsOpen ? "sidebar-links" : ""} `} />
       </div>
