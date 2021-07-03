f2f3d64c5cbc75fedc2477b8b724bba476fa8ef1
src-dbgr <src.dbgr@gmail.com>
2021-07-03 05:17:45 +0200

    added new fonts

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Layout from "../components/Layout";
+import Hero from "../components/02_hero/Hero";
 
 const index = () => {
   return (
     <>
       <Layout>
+        <Hero />
         <h1>Test</h1>
       </Layout>
     </>
