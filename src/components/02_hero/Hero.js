38d7571cd18cc71b21f8a70928e3acddccc1da8f
src-dbgr <src.dbgr@gmail.com>
2021-07-11 23:28:53 +0200

    extended hero description

--- src/components/02_hero/Hero.js
+++ src/components/02_hero/Hero.js
@@ -9,10 +9,21 @@ const Hero = () => {
         <article className="hero-info">
           <div>
             <div className="underline"></div>
+            <h4>Hi, my name is</h4>
             <h1>
-              I'm <span className="highlight">Samuel</span>
+              <span className="highlight">SAMUEL</span>
             </h1>
-            <h4>developer and machine learning enthusiast</h4>
+            <h2>
+              I BUILD <span className="highlight">IT</span> STUFF
+            </h2>
+            <div className="hero-description">
+              I'm a software developer and machine learning enthusiast who
+              specializes in solving real world IT problems. In my last Job I
+              worked as a Cloud Platform engineer for{" "}
+              <span className="highlight">SAP</span> where I focussed on large
+              scale messaging systems. Owing to my passion and fascination for
+              Machine Learning I decided pursue a career in this area.
+            </div>
             <Link to="/contact" className="btn">
               GET IN TOUCH
             </Link>
