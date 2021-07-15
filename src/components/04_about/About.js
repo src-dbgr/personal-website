35a67018f6f9db055bee8603bc28f0292339a1ac
src-dbgr <src.dbgr@gmail.com>
2021-07-15 17:25:04 +0200

    bug fixes

--- src/components/04_about/About.js
+++ src/components/04_about/About.js
@@ -3,6 +3,8 @@ import Title from "../general/Title";
 import { GatsbyImage, StaticImage, getImage } from "gatsby-plugin-image";
 import { graphql, useStaticQuery } from "gatsby";
 import { BsCircleFill } from "react-icons/bs";
+import { IoTriangleSharp } from "react-icons/io5";
+import { IoSquareSharp } from "react-icons/io5";
 import Aos from "aos";
 import "aos/dist/aos.css";
 
@@ -23,7 +25,7 @@ const About = () => {
 
   return (
     <section id="about" className="section about-component-section">
-      <Title title="About"/>
+      <Title title="About" />
       <div className="section-center about-component-center">
         <article
           key="1"
@@ -31,7 +33,13 @@ const About = () => {
           data-aos="slide-right"
           data-aos-once="true"
         >
-          <BsCircleFill className="about-component-icon" />
+          <div className="ball-icon-wrapper">
+            <BsCircleFill id="bs-circle-fill" className="about-component-icon" />
+            <IoTriangleSharp
+              id="io-triangle-sharp"
+              className="about-component-icon"
+            />
+          </div>
           <h4>Who am I?</h4>
           <div className="underline"></div>
           <p>
@@ -58,26 +66,24 @@ const About = () => {
           data-aos="slide-left"
           data-aos-once="true"
         >
-          <div className="about-hover-wrapper">
-            <div className="about-img">
-              <StaticImage
-                // src="../../assets/images/about/hero-colors-dark-soft-edge.png"
-                src="../../assets/images/about/var_5.png"
-                alt="about-img"
-                className="about-default-img"
-                placeholder="blur-up"
-                formats={["auto", "webp", "avif"]}
-              />
-              <StaticImage
-                // src="../../assets/images/about/hero-colors-green-clean.png"
-                // src="../../assets/images/about/hero-no-fx-3.png"
-                src="../../assets/images/about/var_1.png"
-                alt="about-img"
-                className="about-hover-img"
-                placeholder="blur-up"
-                formats={["auto", "webp", "avif"]}
-              />
-            </div>
+          <div className="about-img">
+            <StaticImage
+              // src="../../assets/images/about/hero-colors-dark-soft-edge.png"
+              src="../../assets/images/about/var_5.png"
+              alt="about-img"
+              className="about-default-img"
+              placeholder="blur-up"
+              formats={["auto", "webp", "avif"]}
+            />
+            <StaticImage
+              // src="../../assets/images/about/hero-colors-green-clean.png"
+              // src="../../assets/images/about/hero-no-fx-3.png"
+              src="../../assets/images/about/var_1.png"
+              alt="about-img"
+              className="about-hover-img"
+              placeholder="blur-up"
+              formats={["auto", "webp", "avif"]}
+            />
           </div>
         </article>
       </div>
