fab1ef409806410e4c0c635772a00ff3a401bb34
src-dbgr <src.dbgr@gmail.com>
2021-07-18 01:45:09 +0200

    added fade in fx

--- src/components/05_projects/Project.js
+++ src/components/05_projects/Project.js
@@ -1,23 +1,35 @@
-import React from "react";
+import React, { useEffect } from "react";
 import PropTypes from "prop-types";
 import { GatsbyImage, getImage } from "gatsby-plugin-image";
 import { FaGithubSquare, FaShareSquare } from "react-icons/fa";
+import { BsCircleFill } from "react-icons/bs";
+import { IoTriangleSharp } from "react-icons/io5";
+import Aos from "aos";
+import "aos/dist/aos.css";
+
 const Project = ({ image, title, description, github, stack, url, index }) => {
-  console.log(image);
+  useEffect(() => {
+    Aos.init({ duration: 500, disable: "mobile" });
+  }, []);
+
   // checks whether an image has been set, if noc image is set, don't render --> lines 9-11
   return (
     <article className="project">
       {image && (
         // <Image fluid={image.childImageSharp.fluid} className="project-img" />
-        <GatsbyImage
-          image={getImage(image.localFile)}
-          className="project-img shadow-box-dark"
-          alt={title}
-          //   formats={["avif", "webp", "auto"]}
-        />
+        <div className="project-img" data-aos="slide-right" data-aos-once="true">
+          <GatsbyImage
+            image={getImage(image.localFile)}
+            className="project-img shadow-box-dark"
+            alt={title}
+            //   formats={["avif", "webp", "auto"]}
+          />
+        </div>
       )}
-      <div className="project-info">
-        <span className="project-number">0{index + 1}.</span>
+      <div className="project-info" data-aos="slide-left" data-aos-once="true">
+        <span className="project-number">
+          {index % 2 === 0 ? <BsCircleFill /> : <IoTriangleSharp />}
+        </span>
         <h3>{title || "Title Prop not set"}</h3>
         <p className="project-desc">{description}</p>
         <div className="project-stack">
